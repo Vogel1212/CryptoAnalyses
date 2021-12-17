98080793c5697e2e22a3cbb8e1d57f9a7d193a62
Gabriel Vogel <vogel.gabriel@hotmail.com>
2021-12-17 01:51:55 -0300

    Init Code

---
+++ cryptoAnalyse.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from tkinter import ttk
+from PIL import ImageTk, Image
+
+import json
+import requests
+
+#colors
+black = "#444466"
+white = "#feffff"
+blue = "#6f9fbd"
+screen = "#0e0f13"
+##
+
+window = Tk()
+window.txt('')
+window.geometry('350x350')
+window.configure(background=screen)
